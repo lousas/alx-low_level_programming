15c5b3c1dd9b154e546a244fb75e8456a6c5b15e
lousas <belhajlouay5@gmail.com>
2023-10-06 15:31:42 -0700

    a

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,22 +3,22 @@
 /**
  * main - Entry point
  *
- * Description: Prints all single-digit numbers of base 10 followed by a new line.
+ * Description: Prints all digit numbers of base 10 followed by a new line.
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-    int digit = 0;
+	int digit = 0;
 
-    while (digit < 10)
-    {
-        putchar(digit + '0'); // Convert the digit to its ASCII character representation
-        digit++;
-    }
+	while (digit < 10)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
 
