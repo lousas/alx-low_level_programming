f3b41f4e03af631b3e81546dfd7e6e093090cf2a
lousas <belhajlouay5@gmail.com>
2023-10-09 03:36:35 -0700

    a

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible different combinations of three digits.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int hundreds_digit, tens_digit, ones_digit;
+
+	for (hundreds_digit = 0; hundreds_digit <= 7; hundreds_digit++)
+	{
+		for (tens_digit = hundreds_digit + 1; tens_digit <= 8; tens_digit++)
+		{
+			for (ones_digit = tens_digit + 1; ones_digit <= 9; ones_digit++)
+			{
+				putchar(hundreds_digit + '0');
+				putchar(tens_digit + '0');
+				putchar(ones_digit + '0');
+
+				if (hundreds_digit != 7 || tens_digit != 8 || ones_digit != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
