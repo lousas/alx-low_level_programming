bd658cf5b22f5a060c56129af5fed69e34339c33
lousas <belhajlouay5@gmail.com>
2023-10-10 10:49:35 -0700

    a

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,22 @@
 #include <stdio.h>
 int _putchar(char c);
 
+void print_alphabet (void); void print_alphabet_x10(void);
 
+int is lower (int c);
+
+int_isalpha(int c);
+
+int print sign(int n);
+
+int abs (int);
+
+int print_last_digit(int);
+
+void jack_bauer (void); void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
 
 #endif
